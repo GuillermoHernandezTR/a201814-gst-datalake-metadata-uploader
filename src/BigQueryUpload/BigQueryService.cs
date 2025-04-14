02b4c46a45fd24a9b8cce0c9323dd5d9d470c7c3
JoseGuillermoHernandezGuerrero <Guillermo.Hernandez@thomsonreuters.com>
2025-04-14 12:11:39 -0600

    Update XML upload functionality to include multiple directory paths and adjust BigQuery insert error handling

--- src/BigQueryUpload/BigQueryService.cs
+++ src/BigQueryUpload/BigQueryService.cs
@@ -64,7 +64,7 @@ namespace BigQueryUpload
                 {
                     Console.WriteLine($"Uploading final batch of {rowsBatch.Count} rows to BigQuery.");
                     var insertRows = _client.InsertRows(tableReference, rowsBatch);
-                    if (insertRows != null)
+                    if (insertRows.Errors != null && insertRows.Errors.Count() > 0)
                     {
                         Console.WriteLine($"Errors while inserting rows into BigQuery: {insertRows}");
                         throw new Exception($"Errors while inserting rows into BigQuery: {insertRows}");
@@ -79,21 +79,28 @@ namespace BigQueryUpload
         {
             int currentId = idCounter++; // Assign a unique ID to the current node
 
+            // Convert attributes to a dictionary
+            var attributesDict = new Dictionary<string, string>();
+            foreach (System.Xml.XmlAttribute attribute in element.Attributes)
+            {
+                attributesDict[attribute.Name] = attribute.Value;
+            }
+
             // Add the current node as a row
             var row = new BigQueryInsertRow
             {
                 { "id", currentId },
                 { "parent_id", parentId },
                 { "tag", element.Name },
-                { "attributes", JsonConvert.SerializeObject(element.Attributes) }, // Serialize attributes as JSON string
+                { "attributes", JsonConvert.SerializeObject(attributesDict) }, // Serialize attributes dictionary as JSON string
                 { "text", element.InnerText.Trim() },
-                { "file_name", Path.GetFileName(filePath) },
+                { "file_name", filePath },
                 { "modification_time", modificationTime.ToUniversalTime() }
             };
             rowsBatch.Add(row);
 
             // If the batch size reaches 1000, upload it to BigQuery
-            if (rowsBatch.Count >= 1000)
+            if (rowsBatch.Count >= 10000)
             {
                 Console.WriteLine($"Uploading batch of {rowsBatch.Count} rows to BigQuery.");
                 var insertRows = _client.InsertRows(tableReference, rowsBatch);
--- src/BigQueryUpload/XmlUploader.cs
+++ src/BigQueryUpload/XmlUploader.cs
@@ -16,7 +16,26 @@ namespace BigQueryUpload
 
         public void UploadXml(string directoryPath, string datasetId, string tableId)
         {
-            List<string> files = Directory.GetFiles(directoryPath, "*.xsd").ToList();
+            List<string> files = new List<string>();
+            string[] directories = new string[]
+            {
+                @"//cisprod-0301.int.thomsonreuters.com/taxapptech$/TaxApps/2024/1040ta4/EFile",
+                @"//cisprod-0301.int.thomsonreuters.com/taxapptech$/TaxApps/2024/1041ta4/EFile",
+                @"//cisprod-0301.int.thomsonreuters.com/taxapptech$/TaxApps/2024/1065ta4/EFile",
+                @"//cisprod-0301.int.thomsonreuters.com/taxapptech$/TaxApps/2024/1120ta4/EFile",
+                @"//cisprod-0301.int.thomsonreuters.com/taxapptech$/TaxApps/2024/5500ta4/EFile",
+                @"//cisprod-0301.int.thomsonreuters.com/taxapptech$/TaxApps/2024/7060ta4/EFile",
+                @"//cisprod-0301.int.thomsonreuters.com/taxapptech$/TaxApps/2024/7090ta4/EFile",
+                @"//cisprod-0301.int.thomsonreuters.com/taxapptech$/TaxApps/2024/F990ta4/EFile"
+            };
+
+            foreach (string dir in directories)
+            {
+                if (Directory.Exists(dir))
+                {
+                    files.AddRange(Directory.GetFiles(dir, "*.xsd"));
+                }
+            }
             foreach (string file in files)
             {
                 _bigQueryService.UploadXmlToBigQuery(file, datasetId, tableId);
