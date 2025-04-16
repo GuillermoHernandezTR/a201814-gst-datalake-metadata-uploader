7ea1e31e9f7a146bd270f015c8878675320db4df
JoseGuillermoHernandezGuerrero <Guillermo.Hernandez@thomsonreuters.com>
2025-04-16 09:14:07 -0600

    Enhance logging during file retrieval and data extraction in UploadEfileJurisdictionSchemaAsync; improve error messages for directory access issues.

--- src/BigQueryUpload/XmlUploader.cs
+++ src/BigQueryUpload/XmlUploader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Google.Apis.Bigquery.v2.Data;
 
 namespace BigQueryUpload
 {
@@ -19,50 +20,72 @@ namespace BigQueryUpload
         {
             string datasetId = "mdr";
             string tableId = "efile_xsd";
+            
+            // Verificar y crear la tabla si es necesario
+            TableReference tableReference = await _bigQueryService.EnsureTableExistsAsync(datasetId, tableId);
+            
+            DateTime lastModifiedTime = await XmlUploader.GetMaxModifiedTimeAsync(tableReference);
             List<string> files = new List<string>();
             string baseDirectory = @"//cisprod-0301.int.thomsonreuters.com/taxapptech$/TaxApps";
             string directory = $@"{baseDirectory}/{year}/{taxreturntype}ta{year.Last()}/EFile";
 
-            // Obtener todos los archivos de manera asíncrona
-            files = (await GetFilesWithExtensionAsync(directory, ".xsd")).ToList();
-            Console.WriteLine($"Total files found: {files.Count}");
+            // Log the start of file retrieval
+            Console.WriteLine("Starting to retrieve files...");
+            var startTime = DateTime.Now;
 
-            // Verificar y crear la tabla si es necesario
-            var tableReference = await _bigQueryService.EnsureTableExistsAsync(datasetId, tableId);
+            // Retrieve all files asynchronously
+            files = (await GetFilesWithExtensionAsync(directory, ".xsd")).ToList();
+
+            // Log the end of file retrieval
+            var endTime = DateTime.Now;
+            Console.WriteLine($"File retrieval completed. Time elapsed: {(endTime - startTime).TotalSeconds} seconds. Total files: {files.Count}");
+
+
+            int totalFiles = files.Count;
+
+            // Log the start of data extraction
+            Console.WriteLine("Starting data extraction and upload to BigQuery...");
+            var extractionStartTime = DateTime.Now;
 
             await Parallel.ForEachAsync(files, async (file, _) =>
             {
-                await _bigQueryService.UploadXmlToBigQueryAsync(file, tableReference); // Pasar datasetId y tableId directamente
+                await _bigQueryService.UploadXmlToBigQueryAsync(file, tableReference);
             });
+
+            // Log the end of data extraction
+            var extractionEndTime = DateTime.Now;
+            Console.WriteLine($"Data extraction and upload completed. Time elapsed: {(extractionEndTime - extractionStartTime).TotalSeconds} seconds.");
+
         }
 
         static async Task<IEnumerable<string>> GetFilesWithExtensionAsync(string directoryPath, string fileExtension)
         {
             var files = new List<string>();
+            var startTime = DateTime.Now;
 
             try
             {
-                // Obtenemos los subdirectorios de manera asincrónica
+                // Retrieve subdirectories asynchronously
                 var subdirectories = Directory.EnumerateDirectories(directoryPath);
 
                 var tasks = new List<Task<IEnumerable<string>>>();
 
-                // Crea una tarea para cada subdirectorio
+                // Create a task for each subdirectory
                 foreach (var subdirectory in subdirectories)
                 {
                     tasks.Add(Task.Run(() => GetFilesWithExtensionAsync(subdirectory, fileExtension)));
                 }
 
-                // Espera a que todas las tareas se completen
+                // Wait for all tasks to complete
                 var results = await Task.WhenAll(tasks);
 
-                // Agrega los archivos de cada subdirectorio al resultado
+                // Add files from each subdirectory to the result
                 foreach (var result in results)
                 {
                     files.AddRange(result);
                 }
 
-                // Agrega los archivos del directorio actual
+                // Add files from the current directory
                 foreach (var file in Directory.EnumerateFiles(directoryPath, "*" + fileExtension))
                 {
                     files.Add(file);
@@ -70,17 +93,16 @@ namespace BigQueryUpload
             }
             catch (UnauthorizedAccessException e)
             {
-                Console.WriteLine($"Acceso denegado a {directoryPath}: {e.Message}");
+                Console.WriteLine($"Access denied to {directoryPath}: {e.Message}");
             }
             catch (DirectoryNotFoundException e)
             {
-                Console.WriteLine($"Directorio no encontrado: {e.Message}");
+                Console.WriteLine($"Directory not found: {e.Message}");
             }
             catch (IOException e)
             {
-                Console.WriteLine($"Error de E/S: {e.Message}");
+                Console.WriteLine($"I/O error: {e.Message}");
             }
-
             return files;
         }
 
